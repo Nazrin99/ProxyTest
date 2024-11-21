94fcb0fc5c2343a978a0000edadf28428840cfef
nazrin.nazri <nazrin.nazri@skrine.com>
2024-11-21 19:53:27 +0800

    First commit

---
+++ src/main/java/org/example/proxytest/ProxyTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.proxytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyTestApplication.class, args);
+    }
+
+}
